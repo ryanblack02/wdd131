2fe0e996de5cf3bccf41e96ea00b05689b3546eb
Ryan Black <rblack02@byui.edu>
2025-11-04 13:18:47 -0700

    Update courses.js

--- ponder/courses.js
+++ ponder/courses.js
@@ -2,4 +2,11 @@
 const aCourse = {
   code: "CSE121b",
   name: "Javascript Language",
+  sections: [{ sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
+{ sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}]
 };
+aCourse.sections[0].instructor
+const key = "code";
+
+aCourse.name
+aCourse[key]
