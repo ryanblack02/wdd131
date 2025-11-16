e6feea78bd668b44ccad094a6265e469eb8e91c9
Ryan Black <rblack02@byui.edu>
2025-11-15 19:37:04 -0700

    Add initial recipes data to recipes.mjs

---
+++ recipes/recipes.mjs
@@ -0,0 +1,25 @@
+export const recipes = [
+{
+id: 'r1',
+name: 'Spaghetti Bolognese',
+description: 'Classic Italian meat sauce served over spaghetti.',
+image: 'images/spaghetti.jpg',
+rating: 4,
+time: '30 min',
+ingredients: [
+'200g spaghetti',
+'1 cup tomato sauce',
+'200g ground beef',
+'1 onion, diced'
+]
+},
+{
+id: 'r2',
+name: 'Fluffy Pancakes',
+description: 'Light and fluffy pancakes topped with syrup.',
+image: 'images/pancakes.jpg',
+rating: 5,
+time: '20 min',
+ingredients: ['1 cup flour', '1 egg', '1 cup milk', '2 tbsp sugar']
+}
+];
