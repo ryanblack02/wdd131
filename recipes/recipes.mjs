40dcaf64c849914250b1719d0eb28197d921b8e6
Ryan Black <rblack02@byui.edu>
2025-11-15 19:47:48 -0700

    Update recipes with new entries and images

--- recipes/recipes.mjs
+++ recipes/recipes.mjs
@@ -1,25 +1,68 @@
-export const recipes = [
-{
-id: 'r1',
-name: 'Spaghetti Bolognese',
-description: 'Classic Italian meat sauce served over spaghetti.',
-image: 'images/spaghetti.jpg',
+// Updated sample recipes using your actual image filenames
+name: 'Black Beans and Rice',
+description: 'Simple and hearty seasoned black beans served with rice.',
+image: 'images/black-beans-and-rice.jpg',
 rating: 4,
-time: '30 min',
+time: '25 min',
 ingredients: [
-'200g spaghetti',
-'1 cup tomato sauce',
-'200g ground beef',
-'1 onion, diced'
+'1 can black beans',
+'1 cup rice',
+'1/2 onion',
+'Salt and spices'
 ]
 },
 {
-id: 'r2',
-name: 'Fluffy Pancakes',
-description: 'Light and fluffy pancakes topped with syrup.',
-image: 'images/pancakes.jpg',
+id: 'r3',
+name: 'Chicken Curry',
+description: 'Creamy, mildly spicy chicken curry.',
+image: 'images/chicken-curry.webp',
+rating: 5,
+time: '40 min',
+ingredients: ['Chicken', 'Curry paste', 'Coconut milk', 'Onion']
+},
+{
+id: 'r4',
+name: 'Chocolate Chip Cookies',
+description: 'Soft and chewy classic chocolate chip cookies.',
+image: 'images/chocolate-chip-cookies.jpg',
 rating: 5,
 time: '20 min',
-ingredients: ['1 cup flour', '1 egg', '1 cup milk', '2 tbsp sugar']
+ingredients: ['Flour', 'Butter', 'Sugar', 'Chocolate chips']
+},
+{
+id: 'r5',
+name: 'Escalopes de Poulet à la Crème',
+description: 'French-style creamy chicken escalopes.',
+image: 'images/escalopes-de-poulet-a-la-creme.webp',
+rating: 4,
+time: '35 min',
+ingredients: ['Chicken', 'Cream', 'Mushrooms', 'Shallots']
+},
+{
+id: 'r6',
+name: 'German Gooseberry Cake',
+description: 'Traditional sweet and tart gooseberry cake.',
+image: 'images/german-gooseberry-cake.jpg',
+rating: 3,
+time: '50 min',
+ingredients: ['Gooseberries', 'Flour', 'Sugar', 'Eggs']
+},
+{
+id: 'r7',
+name: 'Roasted Potatoes',
+description: 'Crispy, seasoned oven-roasted potatoes.',
+image: 'images/roasted-potatoes.webp',
+rating: 5,
+time: '30 min',
+ingredients: ['Potatoes', 'Olive oil', 'Salt', 'Pepper']
+},
+{
+id: 'r8',
+name: 'Sweet Potato Waffles',
+description: 'Soft waffles made with sweet potatoes.',
+image: 'images/sweet-potato-waffle-md.jpg',
+rating: 4,
+time: '20 min',
+ingredients: ['Sweet potato', 'Eggs', 'Flour', 'Milk']
 }
 ];
