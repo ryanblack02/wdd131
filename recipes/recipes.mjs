5730ec7e97d946f3def693db2f2c1c7b1094a52c
Ryan Black <rblack02@byui.edu>
2025-11-22 11:01:18 -0700

    Add tags to recipe entries
    Updated recipe entries to include tags for categorization.

--- recipes/recipes.mjs
+++ recipes/recipes.mjs
@@ -1,68 +1,72 @@
-// Updated sample recipes using your actual image filenames
-name: 'Black Beans and Rice',
-description: 'Simple and hearty seasoned black beans served with rice.',
-image: 'images/black-beans-and-rice.jpg',
-rating: 4,
-time: '25 min',
-ingredients: [
-'1 can black beans',
-'1 cup rice',
-'1/2 onion',
-'Salt and spices'
-]
-},
-{
-id: 'r3',
-name: 'Chicken Curry',
-description: 'Creamy, mildly spicy chicken curry.',
-image: 'images/chicken-curry.webp',
-rating: 5,
-time: '40 min',
-ingredients: ['Chicken', 'Curry paste', 'Coconut milk', 'Onion']
-},
-{
-id: 'r4',
-name: 'Chocolate Chip Cookies',
-description: 'Soft and chewy classic chocolate chip cookies.',
-image: 'images/chocolate-chip-cookies.jpg',
-rating: 5,
-time: '20 min',
-ingredients: ['Flour', 'Butter', 'Sugar', 'Chocolate chips']
-},
-{
-id: 'r5',
-name: 'Escalopes de Poulet à la Crème',
-description: 'French-style creamy chicken escalopes.',
-image: 'images/escalopes-de-poulet-a-la-creme.webp',
-rating: 4,
-time: '35 min',
-ingredients: ['Chicken', 'Cream', 'Mushrooms', 'Shallots']
-},
-{
-id: 'r6',
-name: 'German Gooseberry Cake',
-description: 'Traditional sweet and tart gooseberry cake.',
-image: 'images/german-gooseberry-cake.jpg',
-rating: 3,
-time: '50 min',
-ingredients: ['Gooseberries', 'Flour', 'Sugar', 'Eggs']
-},
-{
-id: 'r7',
-name: 'Roasted Potatoes',
-description: 'Crispy, seasoned oven-roasted potatoes.',
-image: 'images/roasted-potatoes.webp',
-rating: 5,
-time: '30 min',
-ingredients: ['Potatoes', 'Olive oil', 'Salt', 'Pepper']
-},
-{
-id: 'r8',
-name: 'Sweet Potato Waffles',
-description: 'Soft waffles made with sweet potatoes.',
-image: 'images/sweet-potato-waffle-md.jpg',
-rating: 4,
-time: '20 min',
-ingredients: ['Sweet potato', 'Eggs', 'Flour', 'Milk']
-}
+export const recipes = [
+  {
+    id: 'r1',
+    name: 'Black Beans and Rice',
+    description: 'Simple and hearty seasoned black beans served with rice.',
+    image: 'images/black-beans-and-rice.jpg',
+    rating: 4,
+    time: '25 min',
+    ingredients: ['1 can black beans', '1 cup rice', '1/2 onion', 'Salt and spices'],
+    tags: ['easy', 'vegetarian']
+  },
+  {
+    id: 'r2',
+    name: 'Chicken Curry',
+    description: 'Creamy, mildly spicy chicken curry.',
+    image: 'images/chicken-curry.webp',
+    rating: 5,
+    time: '40 min',
+    ingredients: ['Chicken', 'Curry paste', 'Coconut milk', 'Onion'],
+    tags: ['dinner', 'spicy', 'asian']
+  },
+  {
+    id: 'r3',
+    name: 'Chocolate Chip Cookies',
+    description: 'Soft and chewy classic chocolate chip cookies.',
+    image: 'images/chocolate-chip-cookies.jpg',
+    rating: 5,
+    time: '20 min',
+    ingredients: ['Flour', 'Butter', 'Sugar', 'Chocolate chips'],
+    tags: ['dessert', 'baking']
+  },
+  {
+    id: 'r4',
+    name: 'Escalopes de Poulet à la Crème',
+    description: 'French-style creamy chicken escalopes.',
+    image: 'images/escalopes-de-poulet-a-la-creme.webp',
+    rating: 4,
+    time: '35 min',
+    ingredients: ['Chicken', 'Cream', 'Mushrooms', 'Shallots'],
+    tags: ['french', 'creamy']
+  },
+  {
+    id: 'r5',
+    name: 'German Gooseberry Cake',
+    description: 'Traditional sweet and tart gooseberry cake.',
+    image: 'images/german-gooseberry-cake.jpg',
+    rating: 3,
+    time: '50 min',
+    ingredients: ['Gooseberries', 'Flour', 'Sugar', 'Eggs'],
+    tags: ['dessert', 'german']
+  },
+  {
+    id: 'r6',
+    name: 'Roasted Potatoes',
+    description: 'Crispy, seasoned oven-roasted potatoes.',
+    image: 'images/roasted-potatoes.webp',
+    rating: 5,
+    time: '30 min',
+    ingredients: ['Potatoes', 'Olive oil', 'Salt', 'Pepper'],
+    tags: ['side dish', 'easy']
+  },
+  {
+    id: 'r7',
+    name: 'Sweet Potato Waffles',
+    description: 'Soft waffles made with sweet potatoes.',
+    image: 'images/sweet-potato-waffle-md.jpg',
+    rating: 4,
+    time: '20 min',
+    ingredients: ['Sweet potato', 'Eggs', 'Flour', 'Milk'],
+    tags: ['breakfast', 'sweet']
+  }
 ];
